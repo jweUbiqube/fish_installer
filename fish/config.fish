dedeaaf3b2b7a453b0f537d893ee577363ac9e73
Jérémy Wendels <jeremy.wendels@grenoble-inp.org>
2024-05-21 16:18:44 +0200

    fix: add path in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ set VIRTUAL_ENV_DISABLE_PROMPT "1"
 set -xU MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -xU MANROFFOPT "-c"
 set -x SHELL $HOME/local/bin/fish
+set -x PATH $PATH $HOME/local/bin $HOME/.fzf/bin
 
 ## Export variable need for qt-theme
 if type "qtile" >> /dev/null 2>&1
